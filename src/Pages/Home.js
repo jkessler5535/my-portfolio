94f4d844bcb3c74d6569ab7e3a8054de81ae5b4b
JamieKessler <jkessler5535@gmail.com>
2024-05-03 15:33:25 -0700

    made <h1> bolder also, added no-wrap to <h1>

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
           <Row className="justify-content-center">
             <Col md={6} className="order-md-1 d-flex justify-content-center flex-column align-items-center ">
               <div className="inner-content">
-                <h1>Hi, I'm <span className="my-name">Jamie!</span></h1>
+                <h1>Hi, I'm <span className="my-name">Jamie</span></h1>
                 <h2 className="job-title">Front end Developer</h2>
                 <p>Vista, Ca Based developer. That creates Web applications and responsive user interfaces. </p>
                 <a href="/contact" id="contact-btn" onClick={handleContactClick}>Contact</a>
