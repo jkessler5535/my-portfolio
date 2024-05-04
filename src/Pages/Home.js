a332f5730fc240c122e7df98d43efcdfe38669d5
JamieKessler <jkessler5535@gmail.com>
2024-05-03 19:22:57 -0700

    took vh off of home.js container. added margin to container and padding to .inner-content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import '../Styles/Home.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import '../Images/ImageStyles.css';
 ;
 
@@ -18,16 +18,15 @@ function Home() {
   return (
     <div className="background box-shadow-bottom" id="home">
       <Container className="container-padding hero-banner">
-          <Row className="justify-content-center">
-            <Col md={6} className="order-md-1 d-flex justify-content-center flex-column align-items-center ">
               <div className="inner-content">
                 <h1>Hi, I'm <span className="my-name">Jamie</span></h1>
-                <h2 className="job-title">Front end Developer</h2>
-                <p>Vista, Ca Based developer. That creates Web applications and responsive user interfaces. </p>
-                <a href="/contact" id="contact-btn" onClick={handleContactClick}>Contact</a>
+                <h2 className="job-title">Front-end Developer</h2>
+                <p> I'm a passionate thinker. I put everything I have into everything I do. I like to create cool stuff. So come check out some of my cool stuff.</p>
+                 <p> 
+                  <quote>..."Don't worry if it doesn't work right. If everything did, you'd be out of a job."</quote></p>
+
+                <a href="/contact" id="contact-btn" onClick={handleContactClick}>Check me out</a>
               </div>
-            </Col>
-          </Row>
       </Container>
     </div>
   );
