5df36c2402a44c237d59c4b95a4f6b5f18081373
JamieKessler <jkessler5535@gmail.com>
2024-05-04 18:40:50 -0700

    added styles and content to contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -11,7 +11,8 @@ const Contact = () => {
         <h1 className="logo h1-contact">Contact</h1>
       </div>
       <div className="inner-contact">
-          <p className="contact-p">If your interestd in collaborating dont be shy. Shoot me a message or hit me up on</p>
+          <p className="contact-p">I'm always open to new opportunities and collaborations, so feel free to reach out! Drop me a line at: <a href="mailto:jkessler5535@gmail.com">jkessler5535@gmail.com</a> or connect with
+          me on social media using the links below. I look forward to hearing from you!</p>
       </div>
     </div>
   )
