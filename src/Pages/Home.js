c9fdb96d127cd077d5795cf068472fdb8bd36586
JamieKessler <jkessler5535@gmail.com>
2024-05-05 18:02:43 -0700

    changed btn in my home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -24,7 +24,7 @@ function Home() {
                 <h1>Hi, I'm <span className="my-name">Jamie</span></h1>
                 <h2 className="job-title">Front-end Developer</h2>
                 <p> I'm a passionate creator dedicated to turning ideas into cool projects. Come explore my work and see the cool stuff I make.</p>
-                <a href="/contact" id="contact-btn" onClick={handleContactClick}>Past work</a>
+                <a href="/contact" id="contact-btn" onClick={handleContactClick}>Check me out</a>
               </div>
       </Container>
     </div>
