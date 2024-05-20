ae5c1978f3aa92e42b6c230be8690278b32fe07d
JamieKessler <jkessler5535@gmail.com>
2024-05-20 16:52:08 -0700

    changed projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,22 @@
 import '../Styles/Projects.css';
+import weatherApp from '../Images/project1.png';
+import Soon from '../Images/Soon.jpg';
 
 export default function Projects (){
   return(
     <div className="projects-section" id="projects">
-      <div className="projects-h1">
-        <h1 className="logo h1-projects">Projects</h1>
+      <div className="project-flex">
+          <h1 className="logo h1-projects ">Projects</h1>
+        </div>
+      <div className="project-container">
+        <div className="img-container">
+          <img src={weatherApp} className="project-logo" alt="weather-app"/>
+          <img src={Soon} className="project-logo" alt="weather-app"/>
+          <img src={Soon} className="project-logo" alt="weather-app"/>
+          <img src={Soon} className="project-logo" alt="weather-app"/>
+          <img src={Soon} className="project-logo" alt="weather-app"/>
+          <img src={Soon} className="project-logo" alt="weather-app"/>
+        </div>
       </div>
     </div>
   )
