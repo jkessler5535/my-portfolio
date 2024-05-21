825d8aa2d025a483fce19f06503239964b078f11
JamieKessler <jkessler5535@gmail.com>
2024-05-20 17:18:01 -0700

    added project link to projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,9 +1,27 @@
 import '../Styles/Projects.css';
+import React, {useEffect} from 'react';
 import weatherApp from '../Images/project1.png';
 import Soon from '../Images/Soon.jpg';
 
 
 export default function Projects (){
+  useEffect(() => {
+    const handleImageClick = () => {
+      window.location.href = "https://weather-app-szno-jwft846hq-jamie-kesslers-projects.vercel.app/";
+    };
+
+    const images = document.querySelectorAll('.project-logo');
+    images.forEach(image => {
+      image.addEventListener('click', handleImageClick);
+    });
+
+    return () => {
+      images.forEach(image => {
+        image.removeEventListener('click', handleImageClick);
+      });
+    };
+  }, []);
+
   return(
     <div className="projects-section" id="projects">
        <div className="project-flex">
